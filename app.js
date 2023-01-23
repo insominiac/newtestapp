c50615f8de42175fc130ee275afbb17b5a0b2537
Hemant D <hemant132us@gmail.com>
2023-01-23 20:23:38 +0530

    new version

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
 const express = require('express')
+var axios = require('axios');
 var fs = require('fs');
-//var Git = require("nodegit");
-const app = express()
-const port = 3000
+var base64 = require('base-64');
+let token = "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"
+var name = 'newbot5';
+var htmlcontent = '<html>Whatever</html>'
+var content = base64.encode(htmlcontent);
+let file = fs.writeFile(`${name}.html`, content, (error) => { /* handle error */ });
 
-app.get('/', (req, res) => {
-    var htmlContent = '<html>Whatever</html>';
-    var name = 'newbot';
-    fs.writeFile(`http://emptyminds.in/${name}.html`, htmlContent, (error) => { /* handle error */ });
-    res.status(200).send({
-      message: `Hello from ${name}!`
+console.log(file);
+uploadFileApi(token, content)
 
-      
-    })
-    
-})
+function uploadFileApi(token, content) {
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    var data = JSON.stringify({
+        "message": `${name}`,
+        "content": `${content}`
+    });
+
+    var config = {
+        method: 'put',
+        url: `https://api.github.com/repos/insominiac/dashboard/contents/${name}.html`,
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        },
+        data: data
+    };
+
+    axios(config)
+        .then(function (response) {
+            console.log(JSON.stringify(response.data));
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
